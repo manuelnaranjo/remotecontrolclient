34b90ac3fa1b1dcd11574c103d1fce2dfa438c08
Manuel Francisco Naranjo <manuel@aircable.net>
2011-09-18 14:42:06 -0700

    debianized

---
+++ build-deb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z "$2" ]; then
+    echo "usage $0 PackageName VersionNumber"
+    exit 1
+fi
+
+set -ex
+
+python setup.py clean
+python setup.py sdist
+python setup.py clean
+
+typeset -l lowername
+lowername=$1
+mv dist/$1-$2.tar.gz ../${lowername}_$2.orig.tar.gz
+
+rm -rf dist build
+
+fakeroot dpkg-buildpackage -i.git -uc -us
